568618701f65feead43600895be3169394f12ee1
Alen Komic <akomic@taketheinterview.com>
2015-04-01 10:33:33 +0200

    password_reusable

--- lib/generators/devise_security_ng/templates/password_reusable_migration.rb
+++ lib/generators/devise_security_ng/templates/password_reusable_migration.rb
@@ -1,8 +1,8 @@
 class DeviseAddSecurityNgPasswordReusable<%= table_name.camelize.singularize %> < ActiveRecord::Migration
   def self.up
     create_table :old_passwords do |t|
-      t.integer :user_id
       t.string :encrypted_password
+      t.string :hash_password
       t.string :password_salt
       t.integer :password_reusable_id, null: false
       t.integer :password_reusable_type, null: false
