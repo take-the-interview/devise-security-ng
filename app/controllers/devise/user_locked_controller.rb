cb3645e55b2994f6fd5aca36936e27e4c58f2a83
Alen Komic <akomic@gmail.com>
2016-04-22 12:21:30 +0200

    Adds unlock_token verification

--- app/controllers/devise/user_locked_controller.rb
+++ app/controllers/devise/user_locked_controller.rb
@@ -2,14 +2,16 @@ class Devise::UserLockedController < DeviseController
     prepend_before_filter :require_no_authentication
 
     def show
-        @user = User.find_by_unlock_token(params[:unlock_token])
-        if @user
-            @user.locked_at = nil
-            @user.unlock_token = nil
-            @user.save!
+        if not params[:unlock_token].empty?
+            @user = User.find_by_unlock_token(params[:unlock_token])
+            if @user
+                @user.locked_at = nil
+                @user.unlock_token = nil
+                @user.save!
 
-            flash[:notice] = :unlocked
+                redirect_to new_user_session_path, :notice => I18n.t('unlocked', {:scope => 'devise.success'})
+            end
         end
-        redirect_to new_user_session_path, :notice => I18n.t('unlocked', {:scope => 'devise.success'})
+        redirect_to new_user_session_path
     end
 end
